7b4bde55b6361a8d7b5aeb2b0a88fe1a56030f26
RasmuS2024 <rasburkhanov@yandex.ru>
2024-09-27 21:25:28 +0500

    modified files: linter errors

--- src/Utils.php
+++ src/Utils.php
@@ -3,7 +3,13 @@
 namespace Hexlet\Phpunit\Utils;
 
 // Эта функция переворачивает переданную строку
-function reverseString($string)
+function reverseString(mixed $string): string
 {
+    if (strlen($string) === 0) {
+        return '';
+    }
+    if (strlen($string) === 1) {
+        return $string;
+    }
     return implode(array_reverse(str_split($string)));
 }
--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -3,18 +3,22 @@
 namespace Hexlet\Phpunit\Tests;
 
 use PHPUnit\Framework\TestCase;
+
 use function Hexlet\Phpunit\Utils\reverseString;
 
 // Класс UtilsTest наследует класс TestCase
 // Имя класса совпадает с именем файла
 class UtilsTest extends TestCase
 {
-    public function getFixtureFullPath($fixtureName)
+    public function getFixtureFullPath(string $fixtureName): mixed
     {
+        if (strlen($fixtureName) === 0) {
+            return '';
+        }
         $parts = [__DIR__, 'fixtures', $fixtureName];
         return realpath(implode('/', $parts));
     }
-    
+
     // Метод (функция), определенный внутри класса,
     // Должен начинаться со слова test
     // Ключевое слово public нужно, чтобы PHPUnit мог вызвать этот тест снаружи
